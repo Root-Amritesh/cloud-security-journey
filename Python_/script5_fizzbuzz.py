02b99a05ea53956e5b27cfb8d36a98f829f5371a
Amritesh <amritesh.cybersec@gmail.com>
2026-07-13 16:37:28 -0400

    feat: script5_fizzbuzz.py - unaided 5min

---
+++ Python_/script5_fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Script 5: FizzBuzz."""
+
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+
+        if i % 15 == 0:
+            print("FizzBuzz")
+
+        elif i % 3 == 0:
+            print("Fizz")
+
+        elif i % 5 == 0:
+            print("Buzz")
+
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    fizzbuzz(100)
